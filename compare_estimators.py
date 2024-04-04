bcbe9d04dbbad1f8bf6cc99d103b5d3b7cd0ab4a
Vidas <v3li@uwaterloo.ca>
2024-04-04 12:04:08 +0800

    update

--- compare_estimators.py
+++ compare_estimators.py
@@ -13,19 +13,29 @@ import traceback
 import numpy as np
 import argparse
 
-###############
-root = os.path.join("exp", "compare_estimators") # path to store the results
+############### specify arguments
+root = os.path.join("exp", "compare_estimators")
+# path to store the results
+
 dir_format = "metric={};semivalue={}_{};estimator={}"
-n_process = 100 # number of parallel processes used by runEstimator
-nue_per_proc = 500 # the number of utility evaluations per batch run by each process
-nue_avg = 20000 # the number of utility evaluations on average used to do estimation. The total number is nue_avg * n_valued
-nue_track_avg = 200 # the number of utility evaluations on average to record the estimates. The total number of estimates recorded will be nue_avg // nue_track_avg
+
+n_process = 100
+# number of parallel processes used by runEstimator
+
+nue_per_proc = 500
+# the number of utility evaluations per batch run by each process
+
+nue_avg = 20000
+# the number of utility evaluations on average used to do estimation. The total number is nue_avg * n_valued
+
+nue_track_avg = 200
+# the number of utility evaluations on average to record the estimates. The total number of estimates recorded will be nue_avg // nue_track_avg
 
 seeds = np.arange(30)
 datasets = ["iris", "wind", "MNIST", "FMNIST"]
 metrics = ["accuracy", "cross_entropy"]
-n_valued = 24 # the size of D_{tr}
-n_perf = 24 # the size of D_{perf}
+n_valued = 24  # the size of D_{tr}
+n_perf = 24  # the size of D_{perf}
 semivalues = {
     ("shapley", None) : ["GELS", "GELS_paired", "GELS_shapley", "ARM", "sampling_lift", "kernelSHAP", "kernelSHAP_paired",
                          "unbiased_kernelSHAP", "GELS_shapley_paired", "permutation", "group_testing", "complement",
